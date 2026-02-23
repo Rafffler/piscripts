59079e7c7b49beb102cb5dcd2cd22d02e6a72a14
Rafffler <geis.raphael@web.de>
2026-02-23 15:28:58 +0100

    runninglight

---
+++ Lab3/runningBidirectional.py
@@ -0,0 +1,33 @@
+import time
+import board
+import digitalio
+
+led1 = digitalio.DigitalInOut(board.D2)
+led1.direction = digitalio.Direction.OUTPUT
+led2 = digitalio.DigitalInOut(board.D3)
+led2.direction = digitalio.Direction.OUTPUT
+led3 = digitalio.DigitalInOut(board.D4)
+led3.direction = digitalio.Direction.OUTPUT
+led4 = digitalio.DigitalInOut(board.D17)
+led4.direction = digitalio.Direction.OUTPUT
+
+while True:
+	led1.value = False
+	led2.value = True
+	time.sleep(0.1)
+	led2.value = False
+	led3.value = True
+	time.sleep(0.1)
+	led3.value = False
+	led4.value = True
+	time.sleep(0.1)
+# backwards again
+	led4.value = False
+	led3.value = True
+	time.sleep(0.1)
+	led3.value = False
+	led2.value = True
+	time.sleep(0.1)
+	led2.value = False
+	led1.value = True
+	time.sleep(0.1)
