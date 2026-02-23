f8a2bc415f1f35430874a3e0f518edc406af40c0
Rafffler <geis.raphael@web.de>
2026-02-23 15:49:14 +0100

    lab3.1without8

--- Lab3/Lab3.1.py
+++ Lab3/Lab3.1.py
@@ -11,17 +11,22 @@ led3.direction = digitalio.Direction.OUTPUT
 led4 = digitalio.DigitalInOut(board.D17)
 led4.direction = digitalio.Direction.OUTPUT
 
+delays = [0.5, 0.5, 0.5, 1.5, 1.5, 1.5, 0.5, 0.5, 0.5]
+
 while True:
-	led1.value = True
-	led2.value = True
-	led3.value = True
-	led4.value = True
-	time.sleep(0.1)
-	led1.value = False
-	led2.value = False
-	led3.value = False
-	led4.value = False
-	time.sleep(0.1)
+	for delay in delays:
+		led1.value = True
+		led2.value = True
+		led3.value = True
+		led4.value = True
+		time.sleep(delay)
+		led1.value = False
+		led2.value = False
+		led3.value = False
+		led4.value = False
+		time.sleep(0.5)
+	time.sleep(1)
+
 
 
 
